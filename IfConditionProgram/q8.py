cd534d1af922ce6cf386aadc3ee86cb8eed23677
Sumit212121 <sumitbarnwal844@gmail.com>
2025-09-17 22:01:27 +0530

    to check a person is eligible to vote or not

---
+++ IfConditionProgram/q8.py
@@ -0,0 +1,8 @@
+# 8.wap to check eligible to vote take user input as a age
+
+age=int(input("enter the age :"))
+
+if age < 18 :
+    print("it is not eligigble to vote")
+else:
+    print("it is eligible")
