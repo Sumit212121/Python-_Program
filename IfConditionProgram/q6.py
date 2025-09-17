661dcef12cbcde4bd50f2e05c5a7f8246c390739
Sumit212121 <sumitbarnwal844@gmail.com>
2025-09-17 21:59:00 +0530

    Checking the number is divisible by 5 .

---
+++ IfConditionProgram/q6.py
@@ -0,0 +1,7 @@
+# 6.wap to check if the given number is divisible by 5 (take user input)
+
+num=int(input("enter the number : "))
+if num % 5 ==0:
+    print("it is divisible by 5")
+else:
+    print("it is not")
