87a5daaecce695e7af1e59c184be294e3366d0e7
Sumit212121 <sumitbarnwal844@gmail.com>
2025-09-17 21:57:38 +0530

    Checking then is even or not

---
+++ IfConditionProgram/q5.py
@@ -0,0 +1,9 @@
+# 5.wap to check if the given string has even length of character
+
+
+
+s="hey guys you all are osam"
+if len(s) % 2 == 0:
+    print("yes it contains even number of character")
+else:
+    print("it does not")
