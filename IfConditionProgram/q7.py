0951470d436fb47b15a955e3f352bb81063d858d
Sumit212121 <sumitbarnwal844@gmail.com>
2025-09-17 22:00:15 +0530

    String is present in another string or not

---
+++ IfConditionProgram/q7.py
@@ -0,0 +1,10 @@
+# 7.wap to check if the given programming is present in the list
+
+
+
+p=["java","python","c","c++","RUBy","golang"]
+s='programming'
+if s in p:
+    print("yes pressent ")
+else:
+    print("not present")
