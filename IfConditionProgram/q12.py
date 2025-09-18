15d9f158608332401881d6dab4c4e7eaff0bb46a
Sumit212121 <sumitbarnwal844@gmail.com>
2025-09-18 21:48:10 +0530

    To check given string is upper case or not

---
+++ IfConditionProgram/q12.py
@@ -0,0 +1,7 @@
+# 12.wap to check the given string is uppercase or not (take user input)
+
+st=input("enter the string: ")
+if st.isupper():
+    print(st)
+else:
+    print("it is in lower case")  
