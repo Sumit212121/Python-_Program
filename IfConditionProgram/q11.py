2d233abc6c8aeef4d4f3e3652f9b5c99ebcfdde3
Sumit212121 <sumitbarnwal844@gmail.com>
2025-09-18 21:47:02 +0530

    To check first letter is consonent or not

---
+++ IfConditionProgram/q11.py
@@ -0,0 +1,8 @@
+# 11.wap to check if the first letter in the given string is consonent
+
+
+s="lahari is a good student"
+if s[0] not in 'aeiou':
+    print("it is consonant")
+else:
+    print("it is vowel")
